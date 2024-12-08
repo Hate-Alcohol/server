865e30313be71b09cc14f8a9be1035d357ea9b4c
sangxxjin <sj_0368@naver.com>
2024-12-08 18:04:27 +0900

    setting

---
+++ src/main/java/org/example/hatealcohol/HateAlcoholApplication.java
@@ -0,0 +1,13 @@
+package org.example.hatealcohol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateAlcoholApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateAlcoholApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hatealcohol/HateAlcoholApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hatealcohol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateAlcoholApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
