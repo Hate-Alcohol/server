58db08ab72ce7d286013621b53da730e80481451
sangxxjin <sj_0368@naver.com>
2024-12-08 19:49:26 +0900

    feat: UserDTO 구현

---
+++ src/main/java/org/example/hatealcohol/Oauth2/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package org.example.hatealcohol.Oauth2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
