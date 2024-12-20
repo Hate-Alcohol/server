f26336139be7d5b3bc29f8da0ddada92b74a4514
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-20 15:56:42 +0900

    feat: Redis crud를 위한 유틸 클래스 추가

---
+++ src/main/java/org/example/hatealcohol/gps/config/redis/RedisUtil.java
@@ -0,0 +1,12 @@
+package org.example.hatealcohol.gps.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+  private final RedisTemplate<String, Object> redisTemplate;
+}
