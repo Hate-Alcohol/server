cbdfa2775bc5e7231c37eef464db43c875d2b0d1
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-20 17:39:07 +0900

    feat: Redis crud 로직 추가

--- src/main/java/org/example/hatealcohol/gps/config/redis/RedisUtil.java
+++ src/main/java/org/example/hatealcohol/gps/config/redis/RedisUtil.java
@@ -1,6 +1,10 @@
 package org.example.hatealcohol.gps.config.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.hatealcohol.gps.dto.LocationHistoryRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,4 +13,31 @@ import org.springframework.stereotype.Component;
 public class RedisUtil {
 
   private final RedisTemplate<String, Object> redisTemplate;
+
+  public void saveLocationHistory(Long sessionId, LocationHistoryRequest locationRequest) {
+
+    String key = createLocationHistoryKey(sessionId);
+    redisTemplate.opsForList().rightPush(key, locationRequest); // 순차 저장
+  }
+
+  public List<LocationHistoryRequest> getLocationHistory(Long sessionId) {
+
+    String key = createLocationHistoryKey(sessionId);
+    List<Object> redisData = redisTemplate.opsForList().range(key, 0, -1); // 처음부터 끝 인덱스
+
+    ObjectMapper mapper = new ObjectMapper();
+    List<LocationHistoryRequest> historyRequest = new ArrayList<>();
+
+    for (Object obj : redisData) {
+      historyRequest.add(
+          mapper.convertValue(obj, LocationHistoryRequest.class)
+      );
+    }
+
+    return historyRequest;
+  }
+
+  private String createLocationHistoryKey(Long sessionId) {
+    return "location_history:" + sessionId;
+  }
 }
