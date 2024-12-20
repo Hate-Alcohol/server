14d89c6d0cbcc49de8b664da1757537a16e700f5
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-20 17:18:02 +0900

    feat: 웹소켓세션 아이디 에러 처리 클래스 추가

---
+++ src/main/java/org/example/hatealcohol/gps/exception/SocketIdNullException.java
@@ -0,0 +1,8 @@
+package org.example.hatealcohol.gps.exception;
+
+public class SocketIdNullException extends RuntimeException {
+
+  public SocketIdNullException(String message) {
+    super(message);
+  }
+}
