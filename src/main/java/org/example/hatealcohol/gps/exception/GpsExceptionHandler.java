60e5e2a4803731c25b45361f0e81e520a0da710f
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-27 15:18:37 +0900

    feat: 예외 처리 핸들러 예외 처리 및 응답 데이터 로직 추가

--- src/main/java/org/example/hatealcohol/gps/exception/GpsExceptionHandler.java
+++ src/main/java/org/example/hatealcohol/gps/exception/GpsExceptionHandler.java
@@ -4,10 +4,24 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GpsExceptionHandler {
+
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> catchRedisGetObjectListException(RedisGetObjectListException e) {
+    return new ResponseEntity<>(buildErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private ErrorResponse buildErrorResponse(String message) {
+    return ErrorResponse.builder()
+        .message(message)
+        .build();
+  }
 
   @Getter
   @NoArgsConstructor
