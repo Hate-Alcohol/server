13f7673c13930795d1310bdac35f6cde9335c4bb
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-27 15:17:59 +0900

    feat: Redis 리스트 조회 예외 처리 클래스 추가

---
+++ src/main/java/org/example/hatealcohol/gps/exception/RedisGetObjectListException.java
@@ -0,0 +1,8 @@
+package org.example.hatealcohol.gps.exception;
+
+public class RedisGetObjectListException extends RuntimeException {
+
+  public RedisGetObjectListException(String message) {
+    super(message);
+  }
+}
