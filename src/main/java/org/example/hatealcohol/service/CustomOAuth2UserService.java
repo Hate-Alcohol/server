d7bfd9b07912092e16d81e051d8eacf5612ca87f
sangxxjin <sj_0368@naver.com>
2025-01-02 00:52:13 +0900

    feat: 구글 로그인 성공 데이터 저장

--- src/main/java/org/example/hatealcohol/service/CustomOAuth2UserService.java
+++ src/main/java/org/example/hatealcohol/service/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package org.example.hatealcohol.service;
 
 import org.example.hatealcohol.dto.CustomOAuth2User;
+import org.example.hatealcohol.dto.GoogleResponse;
 import org.example.hatealcohol.dto.NaverResponse;
 import org.example.hatealcohol.dto.OAuth2Response;
 import org.example.hatealcohol.dto.UserDTO;
@@ -31,8 +32,10 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response = null;
         if (registrationId.equals("naver")) {
-
             oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        if (registrationId.equals("google")) {
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
         } else {
 
             return null;
