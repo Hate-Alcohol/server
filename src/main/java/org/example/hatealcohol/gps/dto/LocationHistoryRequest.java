2ac129e96c94faf6c43b0b1c6838cccf7e6f9ff2
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-05 15:01:44 +0900

    feat: 그냥 주석

--- src/main/java/org/example/hatealcohol/gps/dto/LocationHistoryRequest.java
+++ src/main/java/org/example/hatealcohol/gps/dto/LocationHistoryRequest.java
@@ -11,13 +11,12 @@ import lombok.NoArgsConstructor;
 public class LocationHistoryRequest implements Serializable {
 
 //  private Long sessionId; -> key 값으로 쓸 예정
-  private Double latitude;
-  private Double longitude;
-  private LocalDateTime timestamp;
+  private Double latitude;        // 위도
+  private Double longitude;       // 경도
+  private LocalDateTime timestamp;// 타임스탬프
 
   @Builder
-  public LocationHistoryRequest(Double latitude, Double longitude,
-      LocalDateTime timestamp) {
+  public LocationHistoryRequest(Double latitude, Double longitude, LocalDateTime timestamp) {
     this.latitude = latitude;
     this.longitude = longitude;
     this.timestamp = timestamp;
