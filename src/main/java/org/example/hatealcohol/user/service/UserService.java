f2d4e6ad09d11c664928608eacc3a97e4535fc47
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-07 18:39:04 +0900

    feat: User 리포지토리 추가

---
+++ src/main/java/org/example/hatealcohol/user/service/UserService.java
@@ -0,0 +1,12 @@
+package org.example.hatealcohol.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.hatealcohol.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+}
