75fa9aa7089df8648a4a611bdd8bdedd924fa096
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-13 14:42:58 +0900

    feat: 웹소켓 메시지 처리 에러 클래스 추가

---
+++ src/main/java/org/example/hatealcohol/gps/exception/WebSocketHandleMessageException.java
@@ -0,0 +1,8 @@
+package org.example.hatealcohol.gps.exception;
+
+public class WebSocketHandleMessageException extends RuntimeException {
+
+  public WebSocketHandleMessageException(String message) {
+    super(message);
+  }
+}
