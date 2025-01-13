801b768e9537b5d38b91bc26bd2a2affca58caaa
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-13 14:51:00 +0900

    feat: 예외 처리 로직 추가

--- src/main/java/org/example/hatealcohol/gps/exception/GpsExceptionHandler.java
+++ src/main/java/org/example/hatealcohol/gps/exception/GpsExceptionHandler.java
@@ -17,6 +17,11 @@ public class GpsExceptionHandler {
     return new ResponseEntity<>(buildErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> catchInvalidUriException(InvalidUriException e) {
+    return new ResponseEntity<>(buildErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+  }
+
   private ErrorResponse buildErrorResponse(String message) {
     return ErrorResponse.builder()
         .message(message)
