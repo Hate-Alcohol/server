9b2f2f39a527e052fca2e9be9e67ea8b21d5e297
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-13 15:44:03 +0900

    feat: 파싱 에러 클래스 추가

---
+++ src/main/java/org/example/hatealcohol/gps/exception/WebSocketJsonParsingException.java
@@ -0,0 +1,8 @@
+package org.example.hatealcohol.gps.exception;
+
+public class WebSocketJsonParsingException extends RuntimeException {
+
+  public WebSocketJsonParsingException(String message) {
+    super(message);
+  }
+}
