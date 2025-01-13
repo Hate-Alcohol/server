a071d794c836f82b37b00d2caf04b55ab1b1d02c
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-13 15:43:45 +0900

    feat: 웹소켓 유틸 파싱 메서드 추가

--- src/main/java/org/example/hatealcohol/gps/config/websocket/WebSocketUtil.java
+++ src/main/java/org/example/hatealcohol/gps/config/websocket/WebSocketUtil.java
@@ -1,12 +1,19 @@
 package org.example.hatealcohol.gps.config.websocket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.example.hatealcohol.gps.dto.LocationRequest;
 import org.example.hatealcohol.gps.exception.InvalidUriException;
+import org.example.hatealcohol.gps.exception.WebSocketJsonParsingException;
 import org.example.hatealcohol.gps.exception.WebSocketSessionIdNullException;
+import org.springframework.web.socket.WebSocketSession;
 
 public class WebSocketUtil {
+
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   public static void validationUri(URI uri) {
 
@@ -35,5 +42,14 @@ public class WebSocketUtil {
       throw new InvalidUriException("역할이 없습니다.");
     }
     return role;
+  }
+
+  public static LocationRequest toLocationRequest(String payload) {
+
+    try {
+      return objectMapper.readValue(payload, LocationRequest.class);
+    } catch (Exception e) {
+      throw new WebSocketJsonParsingException("웹소켓 ObjectMapper parsing 에러: " + e.getMessage());
+    }
   }
 }
