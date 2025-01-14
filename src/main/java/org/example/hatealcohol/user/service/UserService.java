6135e23e65e27fced8f3cec4582964706af7bb70
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-14 14:40:34 +0900

    feat: 유저 정보 가져오는 메서드 추가

--- src/main/java/org/example/hatealcohol/user/service/UserService.java
+++ src/main/java/org/example/hatealcohol/user/service/UserService.java
@@ -1,6 +1,8 @@
 package org.example.hatealcohol.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.hatealcohol.user.entity.User;
+import org.example.hatealcohol.user.exception.UserNotFoundException;
 import org.example.hatealcohol.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
   private final UserRepository userRepository;
+
+  public User getUserInfo(String userId) {
+
+    Long parseUserId = Long.parseLong(userId);
+
+    User foundUser = userRepository.findById(parseUserId)
+        .orElseThrow(() -> new UserNotFoundException("사용자가 조회되지 않습니다."));
+
+    return foundUser;
+  }
 }
